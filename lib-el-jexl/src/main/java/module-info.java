fe98fd198c48349df75bb6932102ede9eeb1e0f6
Leandro da Silva Vieira <leandrosilvavieir@gmail.com>
2021-11-22 23:42:26 -0300

    ajuste

--- lib-el-jexl/src/main/java/module-info.java
+++ lib-el-jexl/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 import com.lsv.lib.el.jexl.TemplateJexl;
+import com.lsv.lib.template.Template;
 
 module com.lsv.lib.el.jexl {
     requires static lombok;
@@ -7,5 +8,5 @@ module com.lsv.lib.el.jexl {
     requires transitive com.lsv.lib.template;
 
     exports com.lsv.lib.el.jexl;
-    provides com.lsv.lib.template.Template with TemplateJexl;
+    provides Template with TemplateJexl;
 }
